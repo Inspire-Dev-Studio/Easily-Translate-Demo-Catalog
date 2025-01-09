de3f614f6f3552f8c49f21bb5a06a1dedd1c50a7
quickcaptions7 <quickcaptions7@gmail.com>
2025-01-09 12:19:19 +0530

    Initial Commit

---
+++ Easily Translate Demo Catalog/Easily_Translate_Demo_CatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Easily_Translate_Demo_CatalogApp.swift
+//  Easily Translate Demo Catalog
+//
+//  Created by Ajay Mann on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Easily_Translate_Demo_CatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
